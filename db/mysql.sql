4b5b933d21137d590e11240c5c4e876b2819465e
isma <ismaelaqua@hotmail.com>
2025-02-14 14:19:54 +0100

    Modified: db/mysql.sql     Changes: table pets added

--- db/mysql.sql
+++ db/mysql.sql
@@ -15,5 +15,15 @@ CREATE TABLE `daaexample`.`users` (
 	PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `daaexample`.`pets` (
+	`id_pet` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`specie` enum('Dog','Cat','Bird','Rabbit','Other') NOT NULL,
+	`breed` varchar(50),
+	`owner_id` int,
+	PRIMARY KEY (`id_pet`),
+	FOREIGN KEY (`owner_id`) REFERENCES `people`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+
 CREATE USER IF NOT EXISTS 'daa'@'localhost' IDENTIFIED WITH mysql_native_password BY 'daa';
 GRANT ALL ON `daaexample`.* TO 'daa'@'localhost';
